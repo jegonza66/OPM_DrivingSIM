c28a255b420f32471aa9982a5f5019567e84634c
lpajg1 <joaquin.gonzalez6693@gmail.com>
2025-03-20 22:59:47 +0000

    Implemented ICA annotated data loading functions

--- load.py
+++ load.py
@@ -1,3 +1,4 @@
+import functions_analysis
 import paths
 import setup
 import os
@@ -83,30 +84,34 @@ def load_raw_meg_data(subject_id, task, preload=True):
     return raw
 
 
-def preproc_meg_data(subject_id, preload=False):
+def preproc_meg_data(subject_id, task, preload=False):
     """
     Preprocessed MEG data for parent subject as raw instance of MNE.
     """
 
     # Subject preprocessed data path
-    file_path = pathlib.Path(os.path.join(paths.preproc_path, subject_id, f'Subject_{subject_id}_meg.fif'))
+    file_path = pathlib.Path(os.path.join(paths.preproc_path, f'{task}_{subject_id}_bad_ch_removed_meg.fif'))
 
     # Try to load preprocessed data
     try:
         print('\nLoading Preprocessed MEG data')
         meg_data = mne.io.read_raw_fif(file_path, preload=preload)
     except:
-        raise ValueError(f'No previous preprocessed data found for subject {subject_id}')
+        raise ValueError(f'No previous preprocessed data found for subject {subject_id} in {file_path}')
 
     return meg_data
 
 
-def filtered_data(subject_id, band_id, task, method='iir', use_ica_data=True, preload=True, save_data=True):
+def filtered_data(subject_id, band_id, task, method='iir', data_type='ICA', preload=True, save_data=True):
 
-    if use_ica_data:
-        filtered_path = paths.filtered_path_ica + f'{band_id}/{subject_id}/'
+    if data_type == 'ICA':
+        filtered_path = paths.filtered_path_ica + f'{band_id}/'
+    elif data_type == 'ICA_annot':
+        filtered_path = paths.filtered_path_ica_annot + f'{band_id}/'
+    elif data_type == 'RAW':
+        filtered_path = paths.filtered_path_raw + f'{band_id}/'
     else:
-        filtered_path = paths.filtered_path_raw + f'{band_id}/{subject_id}/'
+        raise ValueError(f'Invalid data_type. Should be either ICA, ICA_annot or RAW. Got {data_type} instead')
 
     filtered_meg_data_fname = f'Subject_{subject_id}_method_{method}_meg.fif'
 
@@ -120,10 +125,12 @@ def filtered_data(subject_id, band_id, task, method='iir', use_ica_data=True, pr
         print(f'No previous filtered data found for subject {subject_id} in band {band_id}.\n'
               f'Filtering data...')
 
-        if use_ica_data:
+        if data_type == 'ICA':
             meg_data = ica_data(subject_id=subject_id, task=task, preload=True)
-        else:
-            meg_data = preproc_meg_data(preload=True)
+        elif data_type == 'ICA_annot':
+            meg_data = ica_annot_data(subject_id=subject_id, task=task, preload=True)
+        elif data_type == 'RAW':
+            meg_data = preproc_meg_data(subject_id=subject_id, task=task, preload=True)
 
         l_freq, h_freq = functions_general.get_freq_band(band_id)
         if method:
@@ -142,32 +149,53 @@ def filtered_data(subject_id, band_id, task, method='iir', use_ica_data=True, pr
 
 def ica_data(subject_id, task, preload=True):
 
+    # ICA data path
+    data_path = paths.ica_path + f'{task}_{subject_id}_raw_ica_hfc_meg.fif'
+
     # Try to load ica data
     try:
-        print(f'Loading ica data for subject {subject_id}')
+        print(f'Loading ICA data for subject {subject_id}')
         # Load data
-        ica_data = mne.io.read_raw_fif(paths.ica_path + f'{task}_{subject_id}_raw_ica_hfc_meg.fif', preload=preload)
-
+        meg_data = mne.io.read_raw_fif(data_path, preload=preload)
     except:
-        raise ValueError(f'No previous ica data found for subject {subject_id}')
+        raise ValueError(f'No previous data found for subject {subject_id}')
 
-    return ica_data
+    return meg_data
 
 
-def meg(subject_id, task, data_type='ICA', band_id=None, filter_sensors=True, filter_method='iir', save_data=True):
+def ica_annot_data(subject_id, task, sds=4, preload=True, save_data=True):
 
-    if data_type == 'ICA':
-        if band_id and filter_sensors:
-            meg_data = filtered_data(subject_id=subject_id, task=task, band_id=band_id, save_data=save_data,
-                                     method=filter_method)
-        else:
-            meg_data = ica_data(subject_id=subject_id, task=task, preload=True)
-    elif data_type == 'RAW':
-        if band_id and filter_sensors:
-            meg_data = filtered_data(subject_id=subject_id, task=task, band_id=band_id, use_ica_data=False,
-                                     save_data=save_data, method=filter_method)
-        else:
-            meg_data = subject_id.load_preproc_meg_data()
+    # ICA data path
+    data_fname =  f'{task}_{subject_id}_raw_ica_hfc_meg_annot_{sds}.fif'
+
+    # Try to load ica data
+    try:
+        print(f'Loading data for subject {subject_id}')
+        # Load data
+        annot_data = mne.io.read_raw_fif(paths.ica_annot_path + data_fname, preload=preload)
+    except:
+        print(f'No previous ICA annotated data found for subject {subject_id} in {paths.ica_annot_path + data_fname}')
+        print(f'Running ICA and annotating data with default parameters {sds} SD...')
+
+        # Load ICA data
+        meg_data = ica_data(subject_id=subject_id, task=task, preload=True)
+
+        # Annotate bad segments as per default parameters
+        annot_data, bad_segments = functions_analysis.annotate_bad_intervals(meg_data, data_fname=data_fname, sds=sds, save_data=save_data)
+
+        # Plot bad segments
+        # fig = plot_general.bad_segments(meg_data=annot_data, bad_segments=bad_segments, sds=sds)
+
+    return annot_data
+
+
+def meg(subject_id, task, data_type='ICA', band_id=None, filter_sensors=True, filter_method='iir', preload=True, save_data=True):
+
+    if band_id and filter_sensors:
+        meg_data = filtered_data(subject_id=subject_id, task=task, data_type=data_type, band_id=band_id,
+                                 save_data=save_data, method=filter_method)
+    else:
+        meg_data = ica_data(subject_id=subject_id, task=task, preload=preload)
 
     return meg_data
 
