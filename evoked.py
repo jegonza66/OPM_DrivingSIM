56208328e6001fd12925e6bb14f8b746a85385b8
Joac <joaquin.gonzalez6693@gmail.com>
2025-09-03 13:46:15 -0300

    pick channels corrected path

--- evoked.py
+++ evoked.py
@@ -27,15 +27,15 @@ else:
 #----- Parameters -----#
 task = 'DA'
 # Trial selection
-trial_params = {'epoch_id': 'saccade',  # use'+' to mix conditions (red+blue)
-                'reject': None,  # None to use default {'mag': 5e-12} / False for no rejection / 'subject' to use subjects predetermined rejection value
+trial_params = {'epoch_id': 'pur',  # use'+' to mix conditions (red+blue)
+                'reject': False,  # None to use default {'mag': 5e-12} / False for no rejection / 'subject' to use subjects predetermined rejection value
+                'evt_from_df': True # If True, use events from df, otherwise use events from annotations
                 }
-
 meg_params = {'chs_id': 'mag',
               'band_id': None,
               'filter_sensors': True,
               'filter_method': 'iir',
-              'data_type': 'ICA'
+              'data_type': 'processed_annot',  # 'raw', 'ICA', 'processed', 'tsss'
               }
 
 l_freq, h_freq = functions_general.get_freq_band(band_id=meg_params['band_id'])
@@ -52,17 +52,14 @@ for subject_id in exp_info.subjects_ids:
     subject = setup.subject(subject_id=subject_id)
 
     # Get time windows from epoch_id name
-    map = dict(fixation={'tmin': -0.3, 'tmax': 0.6, 'plot_xlim': (-0.3, 0.6)},
-               saccade={'tmin': -0.3, 'tmax': 0.6, 'plot_xlim': (-0.3, 0.6)},
-               hl_start={'tmin': -3, 'tmax': 35, 'plot_xlim': (-2.5, 33)})
+    map = dict(hl_start={'tmin': -3, 'tmax': 35, 'plot_xlim': (-2.5, 33)})
     tmin, tmax, plot_xlim = functions_general.get_time_lims(epoch_id=trial_params['epoch_id'], subject=subject, plot_edge=0, map=map)
 
     # Baseline
     baseline, plot_baseline = functions_general.get_baseline_duration(epoch_id=trial_params['epoch_id'], tmin=tmin, tmax=tmax)
 
     # Specific run path for saving data and plots
-    save_id = f'{trial_params['epoch_id']}'
-    run_path = f'/Band_{meg_params['band_id']}/{save_id}_{tmin}_{tmax}_bline{baseline}/'
+    run_path = f'/Band_{meg_params['band_id']}/{trial_params['epoch_id']}_task{task}_{tmin}_{tmax}_bline{baseline}/'
 
     # Save data paths
     epochs_save_path = save_path + f'Epochs_{meg_params['data_type']}/' + run_path
@@ -92,23 +89,22 @@ for subject_id in exp_info.subjects_ids:
             # Save data
             if save_data:
                 # Save evoked data
+                os.makedirs(evoked_save_path, exist_ok=True)
                 evoked.save(evoked_save_path + evoked_data_fname, overwrite=True)
         except:
-
             # Load MEG
             meg_data = load.meg(subject_id=subject_id, meg_params=meg_params)
 
             # Epoch data
             epochs, events, onset_times = functions_analysis.epoch_data(subject=subject, epoch_id=trial_params['epoch_id'], meg_data=meg_data,
-                                                           tmin=tmin, tmax=tmax, save_data=save_data,
+                                                           tmin=tmin, tmax=tmax, from_df=trial_params['evt_from_df'], save_data=save_data,
                                                            epochs_save_path=epochs_save_path,
                                                            epochs_data_fname=epochs_data_fname, reject=reject,
                                                            baseline=baseline)
 
             # ----- Evoked -----#
             # Define evoked and append for GA
-            evoked = epochs.average(picks='mag')
-            evokeds.append(evoked)
+            evoked = epochs.average()
 
         if save_data:
             # Save evoked data
@@ -119,7 +115,8 @@ for subject_id in exp_info.subjects_ids:
     evokeds.append(evoked)
 
     # Separete MEG and misc channels
-    evoked_meg = evoked.copy().pick('mag')
+    picks = functions_general.pick_chs(chs_id=meg_params['chs_id'], info=evoked.info)
+    evoked_meg = evoked.copy().pick(picks)
 
     # Save plot
     fname = 'Evoked_' + subject.subject_id + f'_{meg_params['chs_id']}'
@@ -133,6 +130,10 @@ grand_avg = mne.grand_average(evokeds, interpolate_bads=False)
 # Save grand average
 if save_data:
     grand_avg.save(evoked_save_path + grand_avg_data_fname, overwrite=True)
+
+# Separete MEG and misc channels
+picks = functions_general.pick_chs(chs_id=meg_params['chs_id'], info=grand_avg.info)
+grand_avg = grand_avg.copy().pick(picks)
 
 # Plot evoked
 fname = f'Grand_average'
